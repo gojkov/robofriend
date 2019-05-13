5d472df66255bb2cbd63af398eb66eac68db180e
Steve <stevegojkov@gmail.com>
2019-05-13 17:35:02 -0400

    updated robot names

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Dan Priozi',
+    username: 'Dan',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Bran Connors',
+    username: 'Bran',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Jarvis Angle',
+    username: 'Jar',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Alex Varnor',
+    username: 'Alex',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Aron Hutchis',
+    username: 'Aron',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Jeff Thomas',
+    username: 'Jeff',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Stary Oskol',
+    username: 'Red',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'The Hound',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Forrest Bruges',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Wilhelm Morris',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
